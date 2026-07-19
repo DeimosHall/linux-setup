4b96c839d89b494c5f786a72f3d76b0b5c7bdee2
deimos <deimoshall.dev@proton.me>
2026-07-19 08:50:46 -0600

    chore: add git config script

---
+++ git_config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git config --global user.name "deimos"
+git config --global user.email deimoshall.dev@proton.me
+git config --global core.editor "nvim"
+git config --global init.defaultBranch main
+# true for Windows systems and input for Linux and Mac
+# git config --global core.autocrlf true
+git config --global core.autocrlf input
+git config --global credential.helper store
+
+echo "Configuration applied:"
+git config --global --list
